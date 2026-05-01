2d7236c0068b80eb3cf4388608e8ccf67a6375c0
刘浩天 <2410651402@qq.com>
2026-05-02 00:50:41 +0800

    关卡2

--- addons/LevelManeger/加载关卡.gd
+++ addons/LevelManeger/加载关卡.gd
@@ -3,8 +3,8 @@ extends Area2D
 @export_file("*.tscn") var next_level: String
 
 func _ready():
-    self.body_entered.connect(_on_body_entered)
-    
+    body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
     if next_level != "":
-        get_tree().change_scene_to_file(next_level)
+        get_tree().call_deferred("change_scene_to_file", next_level)
