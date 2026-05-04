c77ab6a0ac230091ae8a59db11040f78586cec51
OVOAOVO <2410651402@qq.com>
2026-05-04 12:56:29 +0800

    UI button 切换

--- addons/LevelManeger/加载关卡(UI).gd
+++ addons/LevelManeger/加载关卡(UI).gd
@@ -1,2 +1,10 @@
-extends Node2D
+extends Button
 
+@export_file("*.tscn") var next_level: String
+
+func _ready():
+    pressed.connect(_on_pressed)
+
+func _on_pressed():
+    if next_level != "":
+        get_tree().call_deferred("change_scene_to_file", next_level)
