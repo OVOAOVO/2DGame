8b6f594e06b8ca0454eeb71149efee8eca8884a8
OVOAOVO <2410651402@qq.com>
2026-05-10 16:44:32 +0800

    玩家收击后退

---
+++ GDScripts/玩家/玩家所有移动.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var SPEED: float = 300.0
+@export var JUMP_VELOCITY: float = -850.0
+
+signal move_state_changed(is_running, is_jumping, is_falling, velocity, direction)
+
+@onready var hurtbox = $Area2D
+
+var knockback := Vector2.ZERO
+var knock_time := 0.0
+
+func _on_knockback(dir: Vector2, force: float):
+	knockback = dir * force
+	knock_time = 0.15
+
+func _ready():
+	hurtbox.hit_knockback.connect(_on_knockback)
+
+func _physics_process(delta):
+
+	var direction = Input.get_axis("left", "right")
+
+	# 👉 如果在击退中，优先使用击退
+	if knock_time > 0:
+		velocity = knockback
+		knock_time -= delta
+	else:
+		# 移动
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		# 跳
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# 重力
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+	move_and_slide()
+
+	# 👉 把状态丢出去
+	emit_signal("move_state_changed",
+		abs(velocity.x) > 1,
+		velocity.y < 0,
+		velocity.y > 0 and not is_on_floor(),
+		direction
+	)
--- GDScripts/玩家/玩家收击与碰撞.gd
+++ GDScripts/玩家/玩家收击与碰撞.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 signal touch_enemy
+signal hit_knockback(knock_dir: Vector2, force: float)
+
+@export var knock_force := 400.0
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
@@ -10,3 +13,6 @@ func _on_area_entered(area):
 	if area.is_in_group("Enemy"):
 		print("Enemy碰到玩家了")
 		emit_signal("touch_enemy")
+		# 新信号（击退）
+		var dir = (global_position - area.global_position).normalized()
+		emit_signal("hit_knockback", dir, knock_force)
